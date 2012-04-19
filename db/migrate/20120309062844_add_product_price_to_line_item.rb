4b7c5b58a9b6849132b885eff95fba6873e72620
manish <manijain333@gmail.com>
2012-04-19 17:09:09 +0530

    first_commit

--- db/migrate/20120309062844_add_product_price_to_line_item.rb
+++ db/migrate/20120309062844_add_product_price_to_line_item.rb
@@ -1,16 +1,17 @@
 # excercise ch-10
-#class AddProductPriceToLineItem < ActiveRecord::Migration
-#  def self.up
-#    add_column :line_items, :price, :decimal, :precision => 8, :scale => 2
-#
-#    say_with_time "Updating prices..." do
-#      LineItem.all.each do |lineitem|
-#        lineitem.update_attribute :price, lineitem.product.price
-#      end
-#    end
-#  end
-#
-#  def self.down
-#    remove_column :line_items, :price
-#  end
-#end
+
+class AddProductPriceToLineItem < ActiveRecord::Migration
+  def self.up
+    add_column :line_items, :price, :decimal, :precision => 8, :scale => 2
+
+    say_with_time "Updating prices..." do
+      LineItem.all.each do |lineitem|
+        lineitem.update_attribute :price, lineitem.product.price
+      end
+    end
+  end
+
+  def self.down
+    remove_column :line_items, :price
+  end
+end
